b315b21d4164eb8254f1bd278c5ffc8f868267bc
Abhinav Bhatia <abhinav.bhatia.me@gmail.com>
2023-01-14 17:42:58 +0530

    Dirichlet transitions for Random MDPs, like in RL^2 paper.

--- src/random_mdp.jl
+++ src/random_mdp.jl
@@ -1,7 +1,9 @@
 using Random
+using Distributions
 
 export RandomDiscreteMDP
 
+"""State transitions follow a flat Dirichlet distribution. Rewards are determinisitic R(a|s) and are independently sampled from Normal(1,1) for each instance of the MDP. Each episode starts with state=1 deterministically."""
 mutable struct RandomDiscreteMDP <: AbstractMDP{Int, Int}
     const nstates::Int
     const nactions::Int
@@ -12,12 +14,15 @@ mutable struct RandomDiscreteMDP <: AbstractMDP{Int, Int}
     state::Int
     action::Int
     reward::Float64
-    function RandomDiscreteMDP(nstates, nactions)
-        d₀ = rand(nstates)
-        d₀ /= sum(d₀)
-        T = rand(nstates, nactions, nstates)
-        T ./= sum(T, dims=1)
-        R = rand(nactions, nstates)
+    function RandomDiscreteMDP(rng, nstates, nactions)
+        d₀ = zeros(nstates)
+        d₀[1] = 1
+        T = zeros(nstates, nactions, nstates)
+        for s in 1:nstates
+            T[:, :, s] .= rand(rng, Dirichlet(nstates, 1.0), nactions)
+        end
+        @assert all(sum(T; dims=1) .≈ 1.0)
+        R = 1.0 .+ randn(rng, nactions, nstates)
         return new(nstates, nactions, d₀, T, R, 1, 1, 0)
     end
 end
